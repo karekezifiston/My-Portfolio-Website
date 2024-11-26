9a6e3f8bfd1df0134d17d8bddbd49d678bca5cf4
Fiston <karekezifiston30@gmail.com>
2024-11-26 19:09:54 +0200

    a

--- personal-website/src/Page3/Page33.jsx
+++ personal-website/src/Page3/Page33.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import './Page3.css';
 import kigali_puppies from '../assets/kigali_puppies.png';
-import ecommerce from '../assets/ecommerce.png';
+import ecommerce from '../assets/ecommerce1.png';
 import real_estate from '../assets/real_estate.png';
 import link from '../assets/link.png';
+import food from '../assets/food-ecommerce.png';
 
 
 function Page3() {
@@ -20,10 +21,28 @@ function Page3() {
           <div className="all-inform">
             <div className="name-link">
               <h4>Kigali Puppies</h4>
-              <img src={link} width={20} alt="" />
+              <a href='' className="tooltip-container">
+              <img src={link} width={20} alt=""className="img-link" />
+              <span className="tooltip-text">Link</span>
+              </a>
             </div>
             <p className="pr">An online store dedicated to helping you find  adorable and loving puppies, perfect for bringing joy to your life,
             </p>
+          </div>
+        </div>
+        <div className="div-3">
+          <img src={food} className="project-img2"  alt="" />
+          <div className="all-inform">
+            <div className="name-link">
+            <h4>Food Delivery</h4>
+              <a href='https://food-eco-frontend.onrender.com' className="tooltip-container">
+              <img src={link} width={20} alt=""className="img-link" />
+              <span className="tooltip-text">Link</span>
+              </a>
+            </div>
+            {/* <div >
+            </div> */}
+            <p className="pr">A full-stack food delivery website that offers users an easy way to explore menus, place orders, and make secure payments, ensuring a fast."</p>
           </div>
         </div>
         <div className="div-3">
@@ -31,19 +50,13 @@ function Page3() {
           <div className="all-inform">
             <div className="name-link">
               <h4>E-Commerce</h4>
-              <img src={link} width={20} alt="" />
+              <a href='https://e-commerce-web-r41q.onrender.com' className="tooltip-container">
+              <img src={link} width={20} alt=""className="img-link" />
+              <span className="tooltip-text">Link</span>
+              </a>
+              
             </div>
             <p className="pr">An online clothing store designed to make shopping easy whenever you're in Kigali, providing a convenient way to buy clothes.</p>
-          </div>
-        </div>
-        <div className="div-3">
-          <img src={real_estate} className="project-img2"  alt="" />
-          <div className="all-inform">
-            <div className="name-link">
-              <h4>Real Estate</h4>
-              <img src={link} width={20} alt="" />
-            </div>
-            <p className="pr">"This real estate website, built with a focus on frontend development, simplifies property searches for buying, selling, or renting."</p>
           </div>
         </div>
       </div>
