83e09a301962af4b4d590ec2fc54ad584dd7a1cc
Fiston <karekezifiston30@gmail.com>
2024-12-03 16:48:02 +0200

    a

--- personal-website/src/Page5/Page5.jsx
+++ personal-website/src/Page5/Page5.jsx
@@ -46,26 +46,29 @@ const Footer = () => {
       </div> */}
       <div className='footers'>
       <div className='left-footer'>
-        <h1 className='let'>let's <br />talk <br />here</h1>
-         <div className='talk'>
+        <div>
+        <h1 className='let'>Talk To Me Here...</h1>
+        
+        </div>
+        <div className='right-footer'>
+      <div className='third-way'>
+          <a href="https://mail.google.com/mail/karekezifiston@gmail.com"><img src={email}className='short-icon' alt="" />karekezifiston30@gmail.com</a>
+          <a href=""><img className='short-icon' src={call}  alt="" /> +250 780 377 645</a>
+        </div>
+      </div>
+      </div>
+      <div className='talk'>
          <form onSubmit={handleSubmit}>
          <div className="first">
          <input type="text" name="userName"id='userName' value={formData.userName} placeholder="NAME"onChange={handleChange} required />
          <input type="email" name="userEmail"id='userEmail' value={formData.userEmail} placeholder="EMAIL"onChange={handleChange} required />
          </div>
          <div className="text-area">
-         <textarea name="userMessage"id='userMessage' value={formData.userMessage} rows="6" placeholder="Text"onChange={handleChange}></textarea>
+         <textarea name="userMessage"id='userMessage' value={formData.userMessage} rows="6" placeholder="MESSAGE"onChange={handleChange}></textarea>
          </div>
          <button type="submit" className="submit">Send <img src={send}width={20} alt="" /></button>
          </form>
          </div>
-      </div>
-      <div className='right-footer'>
-      <div className='third-way'>
-          <a href="https://mail.google.com/mail/karekezifiston@gmail.com"><img src={email}className='short-icon' alt="" />karekezifiston30@gmail.com</a>
-          <a href=""><img className='short-icon' src={call}  alt="" /> +250 780 377 645</a>
-        </div>
-      </div>
       </div>
         
       
