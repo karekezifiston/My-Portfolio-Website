4788f9bb3b3696025d8bb3f242115334a5988c20
karekezi fiston <karekezifiston30@gmail.com>
2025-04-14 15:04:36 +0200

    aaa

--- personal-website/src/Header/Header.jsx
+++ personal-website/src/Header/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
         </div>
         <div className="h-container">
          
-        <h3 className="nav_logo"><img src={fiston}width={130} alt="" /></h3>
+        <a href="#home"><h3 className="nav_logo"><img src={fiston}width={130} alt="" /></h3></a>
   
           
         <div className="menu">
