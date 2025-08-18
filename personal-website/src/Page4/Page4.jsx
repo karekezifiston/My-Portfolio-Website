147a0cf01aa57a7c133ad633e0dabbb45d42669b
karekezi fiston <karekezifiston30@gmail.com>
2025-08-18 14:51:48 +0200

    AAAA

--- personal-website/src/Page4/Page4.jsx
+++ personal-website/src/Page4/Page4.jsx
@@ -5,14 +5,14 @@ const Page3 = () => {
 return(
     <section className="page_3" id="services">
         <div className="title">
-        <h1 className="about_me"> Our Services</h1>
+        <h1 className="about_me">  Services</h1>
         <p className="get_to_know">Explore More </p>
       </div>
       <div className="three-container">
           <div className="web-design">
             <img src="web-design.png"width={40}className="img-serv" alt="" />
             <h2 className="text-border">Web Design</h2>
-            <p className="text-p">One of Our services is about creating websites that look great and are easy to use. We can make your site unique, work on any device, and help attract more visitors.</p>
+            <p className="text-p">One of My services is about creating websites that look great and are easy to use. We can make your site unique, work on any device, and help attract more visitors.</p>
 
           </div>
           <div className="web-design">
